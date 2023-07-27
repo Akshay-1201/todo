b38fca7794c36b0e010e96c75666a0e39fb5ecd0
Akshay-1201 <akshaykute12@gmail.com>
2023-07-27 22:11:21 +0530

    commit

---
+++ src/Todo.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const Todo = () => {
+    const [todos,setTodos] = useState([]);
+    const[inputValue,setInputValue] = useState('');
+    const addTodo = ()=>{
+        
+        if(inputValue.trim()!==''){
+            const newTodo = {
+                id: new Date().getTime(),
+                Text: inputValue,
+            }
+            setTodos([...todos,newTodo]);
+            setInputValue('');
+        }
+    }
+  return (
+    <div className='todo-contianer'>
+    <h2>TODO List</h2>
+    <input type='text'value={inputValue} onChange=  {(e)=>setInputValue(e.target.value)}/>
+    <button onClick={addTodo} >Add</button>
+     <ul>
+        {todos.map((todo)=>(
+            <li key={todo.id}>
+            {todo.text}
+            </li>
+        ))}
+     </ul>
+    </div>
+  )
+}
+
+export default Todo
