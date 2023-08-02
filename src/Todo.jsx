2a06765dca03a8748b8b34c3f7ae23a56f69ce30
Akshay-1201 <akshaykute12@gmail.com>
2023-08-02 16:39:47 +0530

    component complete

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react'
 const Todo = () => {
     const [todos,setTodos] = useState([]);
     const[inputValue,setInputValue] = useState('');
+    const [editMode,setEditMode] = useState(false);
+    const [editId,setEditId]=useState(null);
+    const [editValue,setEaditValue]=useState('');
     const addTodo = ()=>{
         
         if(inputValue.trim()!==''){
@@ -14,15 +17,51 @@ const Todo = () => {
             setInputValue('');
         }
     }
+    const deleteTodo = (id)=>{
+        const updateTdos = todos.filter((todo)=> todo.id !== id);
+        setTodos(updateTdos)
+    }
+    const enterEaditMode = (id,text) => {
+       setEditMode(true);
+       setEditId(id);
+       setInputValue(text);
+
+    }
+    const updatetodo = ()=>{
+        const updateTdos = todos.map((todo)=>{
+            if(todo.id === editId){
+                return{...todo,text:editValue};
+            }
+            return todo;
+        })
+        setTodos(updateTdos);
+        setEditMode(false);
+        setEditId(null);
+        setInputValue('')
+    }
   return (
     <div className='todo-contianer'>
     <h2>TODO List</h2>
     <input type='text'value={inputValue} onChange=  {(e)=>setInputValue(e.target.value)}/>
-    <button onClick={addTodo} >Add</button>
+    
+    {
+        editMode ? (
+            <div>
+                <input type='text' value={editValue}
+                onChange={(e)=>setEaditValue(e.target.value)}/>
+                <button onClick={updatetodo}>Update</button>
+            </div>
+        ) : (
+            <button onClick={addTodo}>Add</button>
+        )
+    }
+    {/* <button onClick={addTodo} >Add</button> */}
      <ul>
         {todos.map((todo)=>(
             <li key={todo.id}>
             {todo.text}
+            <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+            <button onClick={()=>enterEaditMode(todo.id, todo.text)}>Edit</button>
             </li>
         ))}
      </ul>
